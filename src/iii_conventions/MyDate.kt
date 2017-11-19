7747c747964ab6db532ca3e944ae8d90647a961a
Andrew Tomash <kpubedko@gmail.com>
2017-11-20 01:19:52 +0200

    #25&26

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -11,3 +11,15 @@ enum class TimeInterval {
 }
 
 class DateRange(val start: MyDate, val endInclusive: MyDate)
+
+operator fun DateRange.contains(date: MyDate) : Boolean{
+    return date >= start && date <= endInclusive
+}
+
+operator fun MyDate.compareTo(other: MyDate): Int {
+    if(year != other.year)
+        return year - other.year
+    if(month != other.month)
+        return month - other.month
+    return dayOfMonth - other.dayOfMonth
+}
